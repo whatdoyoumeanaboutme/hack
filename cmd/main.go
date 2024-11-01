bff14471d498d2a48eb7bc335611af272e7c9957
ssk2 <1sqlroot@gmail.com>
2024-11-01 09:56:12 +0400

    test_5_add_register

--- cmd/main.go
+++ cmd/main.go
@@ -6,19 +6,6 @@ import (
 	"net/http"
 )
 
-// @title       My Awesome API
-// @version     1.0
-// @description An API to demonstrate OpenAPI integration.
-// @termsOfService  http://swagger.io/terms/
-// @contact.name  API Support
-// @contact.email support@swagger.io
-// @license.name  Apache 2.0
-// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host      localhost:8080
-// @BasePath  /
-// @schemes   http
-
 type Credentials struct {
 	Username string
 	Password string
@@ -28,20 +15,45 @@ type WelcomeData struct {
 	Message string
 }
 
-// @Summary Login to the system
-// @Description Logs in a user with given username and password.
-// @Accept  json
-// @Produce json
-// @Param username query string true "Username"
-// @Param password query string true "Password"
-// @Success 200 {object} Credentials
-// @Failure 401 {object} error "Invalid credentials"
-// @Router /login [post]
+type UserData struct {
+	Username string
+	Password string
+}
+
+// Функция для обработки запросов на регистрацию
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		tmpl, err := template.ParseFiles("register.html") // наша регистрация
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, nil)
+	} else if r.Method == http.MethodPost {
+
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		// Проверяем, существует ли пользователь с таким именем
+		// Мб довать бд
+		if _, ok := users[username]; ok {
+			http.Error(w, "Пользователь с таким именем уже существует", http.StatusBadRequest)
+			return
+		}
+
+		// Создаем нового пользователя
+		users[username] = UserData{Username: username, Password: password}
+
+		// Перенаправление на страницу авторизации после успешной регистрации
+		http.Redirect(w, r, "/", http.StatusFound)
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		// Тут мы к файлу идём
-		tmpl, err := template.ParseFiles("login.html")
+		tmpl, err := template.ParseFiles("login.html") // наша авторизация
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -51,28 +63,32 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		username := r.FormValue("username")
 		password := r.FormValue("password")
 
-		// это наш логин и пароль
-		if username == "user" && password == "pass" {
-			// типо если верно велком , а если нет в попу
-			http.Redirect(w, r, "/welcome", http.StatusFound)
+		// Выполняем простую проверку
+		var errorString string
+		if user, ok := users[username]; ok {
+			if user.Password == password {
+				http.Redirect(w, r, "/welcome", http.StatusFound)
+				return
+			} else {
+
+				errorString = "Ошибка авторизации"
+				http.Error(w, errorString, http.StatusUnauthorized)
+				return
+			}
 		} else {
-			http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+			// Пользователь не найден
+			errorString = "Пользователь не найден"
+			http.Error(w, errorString, http.StatusUnauthorized)
+			return
 		}
 	} else {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
-// @Summary Get welcome page
-// @Description Returns the welcome page with a message.
-// @Accept  json
-// @Produce  json
-// @Success 200 {object} WelcomeData
-// @Router /welcome [get]
-
 func welcomeHandler(w http.ResponseWriter, r *http.Request) {
 	// Отображаем страницу приветствия с использованием шаблона
-	data := WelcomeData{Message: "Welcome to the website!"}
+	data := WelcomeData{Message: "Добро пожаловать"}
 	tmpl, err := template.ParseFiles("welcome.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -81,9 +97,12 @@ func welcomeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, data)
 }
 
+var users = make(map[string]UserData)
+
 func main() {
 	http.HandleFunc("/", loginHandler)
 	http.HandleFunc("/welcome", welcomeHandler)
+	http.HandleFunc("/register", registerHandler)
 
 	fmt.Println("Server starting on http://localhost:8080...")
 	http.ListenAndServe(":8080", nil)
