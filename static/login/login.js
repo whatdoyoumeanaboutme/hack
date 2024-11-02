a4e2119078d3a051bbf40128af1b73ea4d2380bc
gyhxrr31 <ivanov.ivan.ivanovich1968@gmail.com>
2024-11-02 20:13:27 +0400

    Delete static folder

--- static/login/login.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    if (!username || !password) {
-      event.preventDefault();
-      alert('Пожалуйста, заполните все поля.');
-    }
-});
